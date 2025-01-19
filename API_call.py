ac23a28c376705d3e286992c0dbd33ac1e9e7d39
hoffi-81 <Tim.hoffm25@gmail.com>
2025-01-19 16:06:31 +0100

    Update API_call.py
    test

--- API_call.py
+++ API_call.py
@@ -16,5 +16,5 @@ for key, values in needed_info.items():
                for subkey, sub_value_list in value.items():
                     for sub_value in sub_value_list:
                          print(sub_value, json_raw[key][subkey][sub_value])
-                    
+print("hello")
 
